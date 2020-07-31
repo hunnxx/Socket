87e3c4f3027df66c361e6ba5ee37f9ec6dd2332d
hunnxx <null.nhkim@gmail.com>
2020-07-31 15:38:06 +0900

    Update some information
    Add a __init__.py file
    Modify some code in the SocketManager.py file

--- Utils.py
+++ Utils.py
@@ -3,9 +3,9 @@ import logging
 import os
 
 parser = argparse.ArgumentParser(description='For Socket Connection')
-parser.add_argument('--host', metavar='HOST', type=str, default='192.168.0.103', help='a string for a host address')
+parser.add_argument('--host', metavar='HOST', type=str, default='localhost', help='a string for a host address')
 parser.add_argument('--port', metavar='PORT', type=int, default=9999, help='a integer for a port number')
-parser.add_argument('--buff', metavar='BUFF', type=int, default=1, help='a buffer size for a socket connection')
+parser.add_argument('--buff', metavar='BUFF', type=int, default=1024, help='a buffer size for a socket connection')
 parser.add_argument('--path_home', metavar='PATH_HOME', type=str, default=os.getenv('HOME'), help='Path of the home directory')
 
 logger = logging.getLogger('Socket Logger')
